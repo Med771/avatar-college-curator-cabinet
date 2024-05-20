139ab40cf4d4af614b2e5ccfe30a507f731246a7
Med771 <mm7737333@gmail.com>
2024-05-20 06:43:30 +0300

    Feature lessons progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgFor } from "@angular/common";
-import { DATA, LESSONS, STUDENTS } from './data/data';
-import {transformation_json} from "./functions/funcs";
+import { LessonsProgressComponent } from "./lessons-progress/lessons-progress.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NgFor],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, LessonsProgressComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  public result: any = transformation_json(DATA, STUDENTS, LESSONS);
 }
 
